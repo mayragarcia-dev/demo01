9761443d355a7c13f7a47223eb6efbe16d5a5529
mayragarcia-dev <mayjulisarojas@gamil.com>
2026-09-17 02:44:43 -0500

    Agregar PerfilRepository

---
+++ src/main/java/com/tecsup/repository/PerfilRepository.java
@@ -0,0 +1,7 @@
+package com.tecsup.repository;
+
+import com.tecsup.models.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+}
